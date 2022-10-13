ed7ff4503860311383d505427ef009ba390b2e27
barerenate <renatw17@uia.no>
2022-10-13 12:18:47 +0200

    test

--- bacit-dotnet.MVC/Controllers/SuggestionsController.cs
+++ bacit-dotnet.MVC/Controllers/SuggestionsController.cs
@@ -1,7 +1,6 @@
 ﻿using bacit_dotnet.MVC.Models.Suggestions;
 using bacit_dotnet.MVC.DataAccess;
 using Microsoft.AspNetCore.Mvc;
-using bacit_dotnet.MVC.DataAccess;
 
 namespace bacit_dotnet.MVC.Controllers
 {
@@ -24,25 +23,13 @@ namespace bacit_dotnet.MVC.Controllers
         [HttpPost]
         public IActionResult Save(SuggestionViewModel model)
         {
-<<<<<<< HEAD
 
             sqlConnector.setSuggestion(model);
             return View(model);
-=======
             if (string.IsNullOrWhiteSpace(model.Name))
                 throw new ArgumentException();
             sqlConnector.SetSuggestion(model);
             return View(model);
-        }
-
-        private readonly ILogger<SuggestionsController> _logger;
-        private readonly ISqlConnector sqlConnector;
-
-        public SuggestionsController(ILogger<SuggestionsController> logger, ISqlConnector sqlConnector)
-        {
-            _logger = logger;
-            this.sqlConnector = sqlConnector;
->>>>>>> 3253d016b1351f525ae0a514e394a62fed68a5ba
         }
     }
     
--- bacit-dotnet.MVC/DataAccess/ISqlConnector.cs
+++ bacit-dotnet.MVC/DataAccess/ISqlConnector.cs
@@ -6,11 +6,6 @@ namespace bacit_dotnet.MVC.DataAccess
     public interface ISqlConnector
     {
         IEnumerable<User> GetUsers();
-<<<<<<< HEAD
         void setSuggestion(SuggestionViewModel model);
-=======
-
-        void SetSuggestion(SuggestionViewModel model);
->>>>>>> 3253d016b1351f525ae0a514e394a62fed68a5ba
     }
 }
